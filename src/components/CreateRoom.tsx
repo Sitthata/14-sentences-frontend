06afb4f2051a1e80bf1906d1dd2bdacb2c168126
Kittipat Bualoylom <110712358+Tonnamkit@users.noreply.github.com>
2023-09-18 16:26:09 +0700

    Add files via upload

--- src/components/CreateRoom.tsx
+++ src/components/CreateRoom.tsx
@@ -1,30 +1,34 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import io from "socket.io-client";
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import io from 'socket.io-client'
 
-const socket = io("http://localhost:8080");
+const socket = io('http://localhost:8080')
 
 const CreateRoom = () => {
-  const [username, setUsername] = useState("");
-  const navigate = useNavigate();
+  const [username, setUsername] = useState('')
+  const navigate = useNavigate()
 
   useEffect(() => {
     // Look for lobbyCreated Event
-    socket.on("lobbyCreated", (roomId: string, hostUser) => {
-      console.log("Lobby Created: ", roomId);
-      console.log("Initial Users: ", hostUser);
-      navigate(`/lobby/${roomId}`);
-    });
-  }, [navigate, username]);
+    socket.on('lobbyCreated', (roomId: string, hostUser) => {
+      console.log('Lobby Created: ', roomId)
+      console.log('Initial Users: ', hostUser)
+      navigate(`/lobby/${roomId}`)
+    })
+  }, [navigate, username])
 
   const handleCreateRoom = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!username) return alert("Please input your username");
-    socket.emit("createLobby", username);
-  };
+    e.preventDefault()
+    if (!username) return alert('Please input your username')
+    socket.emit('createLobby', username)
+  }
+
+  const handleJoinRoom = () => {
+    navigate(`/join-room`)
+  }
 
   return (
-    <form className="flex flex-col" onSubmit={handleCreateRoom}>
+    <form className="flex flex-col">
       <label htmlFor="username">Input your username</label>
       <input
         className="border-2 border-gray-500"
@@ -32,11 +36,14 @@ const CreateRoom = () => {
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
-      <button className="m-5 border" type="submit">
+      <button className="m-5 border" type="submit" onClick={handleCreateRoom}>
         Create Room
       </button>
+      <button className="m-5 border" type="submit" onClick={handleJoinRoom}>
+        Join Room
+      </button>
     </form>
-  );
-};
+  )
+}
 
-export default CreateRoom;
+export default CreateRoom
---
+++ src/routes/create-room/join-room.tsx
@@ -0,0 +1,12 @@
+import JoinRoom from '../../components/JoinRoom'
+
+const JoinExistRoomPage = () => {
+  return (
+    <div className="flex-col h-screen gap-5 flex-center">
+      <h1 className="text-3xl">14 sentence of death</h1>
+      <JoinRoom />
+    </div>
+  )
+}
+
+export default JoinExistRoomPage
