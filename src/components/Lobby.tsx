2bc2398a1b944c9faec975b2d332f9ff6ae6c3b5
Kittipat Bualoylom <110712358+Tonnamkit@users.noreply.github.com>
2023-09-19 14:02:20 +0700

    Update Lobby.tsx

--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -1,37 +1,42 @@
-import { useParams } from 'react-router-dom';
-import { io } from 'socket.io-client';
-import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom"
+import { io } from "socket.io-client"
+import { useEffect, useState } from "react"
 
-const socket = io('http://localhost:8080');
+const socket = io("https://one4-sentences-backend.onrender.com")
 
 type roomUsersType = {
-  id: string;
-  username: string;
-};
+  id: string
+  username: string
+}
 
 const Lobby = () => {
-  const { roomId } = useParams<{ roomId: string }>();
-  const [roomUsers, setRoomUsers] = useState<roomUsersType[]>([]);
+  const { roomId } = useParams<{ roomId: string }>()
+  const [roomUsers, setRoomUsers] = useState<roomUsersType[]>([])
 
   useEffect(() => {
-    console.log('Component mounted');
+    console.log("Component mounted")
     socket.onAny((event, ...args) => {
-      console.log(event, args);
-    });
-    socket.emit('getRoomInfo', roomId);
-    socket.on('roomInfo', (users: roomUsersType[]) => {
-      console.log('roomInfo received');
-      setRoomUsers(users);
-    });
-  }, [roomId]);
+      console.log(event, args)
+    })
+    socket.emit("getRoomInfo", roomId)
+    socket.on("roomInfo", (users: roomUsersType[]) => {
+      console.log("roomInfo received")
+      setRoomUsers(users)
+    })
+  }, [roomId])
   return (
-    <div>
-      <h1>Lobby id: {roomId}</h1>
+    <div className="flex flex-col h-screen gap-5 flex-center">
+      <h1 className="text-3xl">Lobby id: <span className="font-semibold">{roomId}</span></h1>
       {roomUsers.map((user) => (
-        <div key={user.id}>{user.username}</div>
+        <div className="text-2x1" key={user.id}>
+          {user.username}
+        </div>
       ))}
+      <button className="px-14 py-2 font-semibold text-sm bg-blue-600 text-white rounded-full shadow-sm hover:bg-blue-700">
+        <span className="font-bold mx-8">Start</span>
+      </button>
     </div>
-  );
-};
+  )
+}
 
-export default Lobby;
+export default Lobby
